433a964c7dfd8671c41249e932c210321f89a5e8
Vickywane <vickywane@gmail.com>
2020-07-28 01:38:17 +0100

    files

--- model/activity.js
+++ model/activity.js
@@ -1,12 +1,29 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
-    id  : Number , 
-    title : String, 
-    description : String,
-    date : String,
-    time  : Array, 
-    isCompleted : Boolean
-})
+  id: Number,
+  title: {
+    type: String,
+    min: [3, "use a remarkable name"],
+    required: [
+      true,
+      "how would you recognize your activity without it's name?",
+    ],
+  },
+  description: {
+    type: String,
+    min: [10, "add some details for your activity"],
+    required: [true, "what is your activity about?"],
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: Array,
+  isCompleted: {
+    type: Boolean,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model("Post", schema)
+module.exports = mongoose.model("Post", schema);
